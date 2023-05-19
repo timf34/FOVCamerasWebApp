5384bdcb4206941165f0df0e90ad371506e182b7
timf34 <farrelti@tcd.ie>
2023-05-19 15:04:35 +0100

    hardcoded login working (commiting without hardcoded password)

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,38 +1,61 @@
 import React, { useState, useEffect } from 'react';
+import { signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
+import { auth } from './firebase';
 import io from 'socket.io-client';
 
 function App() {
-    const [status, setStatus] = useState(null);
+  const [status, setStatus] = useState(null);
+  const [user, setUser] = useState(null);
 
-    useEffect(() => {
-        const socket = io('http://localhost:5000');
+  useEffect(() => {
+    const socket = io('http://localhost:5000');
 
-        socket.on('connect', () => {
-            console.log('connected to websocket');
-        });
+    socket.on('connect', () => {
+      console.log('connected to websocket');
+    });
 
-        socket.on('status', (data) => {
-            setStatus(data);
-        });
+    socket.on('status', (data) => {
+      setStatus(data);
+    });
 
-        // Clean up on unmount
-        return () => socket.disconnect();
-    }, []);
+    // Clean up on unmount
+    return () => socket.disconnect();
+  }, []);
 
-    return (
-        <div className="App">
-            {status ? (
-                <div>
-                    <p>Device ID: {status.deviceId}</p>
-                    <p>WiFi Status: {status.wifiStatus ? 'Connected' : 'Disconnected'}</p>
-                    <p>Battery Level: {status.batteryLevel}</p>
-                    <p>Temperature: {status.temperature}</p>
-                </div>
-            ) : (
-                <p>Loading status...</p>
-            )}
+  useEffect(() => {
+    onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+  }, []);
+
+  const handleLogin = async (email, password) => {
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+    } catch (error) {
+      console.error('Failed to sign in:', error);
+    }
+  };
+
+  return (
+    <div className="App">
+      {!user ? (
+        <div>
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <button onClick={() => handleLogin('tim@fov.ie', 'password here!')}>Sign In</button>
         </div>
-    );
+      ) : status ? (
+        <div>
+          <p>Device ID: {status.deviceId}</p>
+          <p>WiFi Status: {status.wifiStatus ? 'Connected' : 'Disconnected'}</p>
+          <p>Battery Level: {status.batteryLevel}</p>
+          <p>Temperature: {status.temperature}</p>
+        </div>
+      ) : (
+        <p>Loading status...</p>
+      )}
+    </div>
+  );
 }
 
 export default App;
