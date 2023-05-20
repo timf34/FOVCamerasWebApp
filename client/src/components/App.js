e4788d9ef981fa4cf8693704c0c999618639e23c
timf34 <farrelti@tcd.ie>
2023-05-20 01:42:46 +0100

    removed debugging print statement

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -30,7 +30,6 @@ function App() {
                 }
                 return newStatuses;
             });
-            console.log('status', data);
         });
     
         return () => socket.disconnect();
