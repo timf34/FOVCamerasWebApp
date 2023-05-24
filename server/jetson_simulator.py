035975a09d5d1d962013b8d1b6d2aca533a52c95
timf34 <farrelti@tcd.ie>
2023-05-24 15:01:58 +0100

    added extra websockets code

--- server/jetson_simulator.py
+++ server/jetson_simulator.py
@@ -12,11 +12,12 @@ Run from multiple terminals as follows:
 `python script.py jetson_nano_4`
 """
 
-
 import requests
 import json
-import time
 import random
+import time
+import websocket
+import threading 
 
 def get_wifi_status():
     # randomly return True or False
@@ -30,6 +31,23 @@ def get_temperature():
     # randomly return a number between 0 and 100
     return random.randint(0, 100)
 
+def on_message(ws, message):
+    print(f'Received command: {message}')
+    # TODO: Handle command...
+
+def on_error(ws, error):
+    print(f'Error: {error}')
+
+def on_close(ws):
+    print('Connection closed')
+
+def on_open(ws):
+    def run():
+        while True:
+            time.sleep(1)
+    thread = threading.Thread(target=run)
+    thread.start()
+
 # Device ID as a command line argument
 import sys
 if len(sys.argv) != 2:
@@ -37,6 +55,18 @@ if len(sys.argv) != 2:
     sys.exit(1)
 
 deviceId = sys.argv[1]
+
+# Create a new WebSocket connection
+ws = websocket.WebSocketApp(
+    f"ws://localhost:5000/{deviceId}",
+    on_open=on_open,
+    on_message=on_message,
+    on_error=on_error,
+    on_close=on_close)
+
+# Start the WebSocket connection in a new thread so it doesn't block the main loop
+wst = threading.Thread(target=ws.run_forever)
+wst.start()
 
 while True:
     try:
