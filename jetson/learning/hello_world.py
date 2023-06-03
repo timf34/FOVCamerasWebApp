0219ff8317dcba6dbe221a04fc34c80185851aab
timf34 <farrelti@tcd.ie>
2023-06-03 17:57:03 +0100

    sample jetson files

---
+++ jetson/learning/hello_world.py
@@ -0,0 +1,8 @@
+import sys
+
+if len(sys.argv) > 1:
+    number = sys.argv[1]
+else:
+    number = 42
+
+print("Hello world, the number is:", number)
---
+++ jetson/learning/number_input_loop.py
@@ -0,0 +1,7 @@
+while True:
+    user_input = input("Enter a number (or 'q' to quit): ")
+    
+    if user_input == 'q':
+        break
+    
+    print("The number you entered is", user_input)
