5a3115c8546302e0210b2a5db4ee70952cdf1862
timf34 <farrelti@tcd.ie>
2023-06-04 14:58:30 +0100

    use std input instead of input() for working with our web app

--- jetson/learning/number_input_loop.py
+++ jetson/learning/number_input_loop.py
@@ -1,7 +1,7 @@
+import sys
+
 while True:
-    user_input = input("Enter a number (or 'q' to quit): ")
-    
+    user_input = sys.stdin.readline().strip()  # Read a line from stdin and remove the newline character at the end
     if user_input == 'q':
         break
-    
     print("The number you entered is", user_input)
