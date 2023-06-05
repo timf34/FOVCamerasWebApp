6f5305c13ae82ad3ef397b662916440b3e46c529
timf34 <farrelti@tcd.ie>
2023-06-05 18:19:11 +0100

    added gstreamer pipeline

--- jetson/jetson_stream_simulator.py
+++ jetson/jetson_stream_simulator.py
@@ -2,7 +2,9 @@ import cv2
 import requests 
 
 # Capture video from the webcam
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(f'nvarguscamerasrc !  video/x-raw(memory:NVMM), width=1920, height=1080, format=NV12, framerate=60/1 ! '
+                f'nvvidconv ! video/x-raw, width={str(1920)}, height={str(1080)}, format=BGRx ! '
+                f'videoconvert ! video/x-raw, format=BGR ! appsink')
 
 while True:
     # Read a frame from the camera
