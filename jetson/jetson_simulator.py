59b8c269e93822584c6b16901c08d49ebdcfa385
timf34 <farrelti@tcd.ie>
2023-06-05 17:29:48 +0100

    more fixes in on send input

--- jetson/jetson_simulator.py
+++ jetson/jetson_simulator.py
@@ -80,8 +80,8 @@ class NamespaceHandler(Client):
         # if process is running, send input to it
         if self.process is not None and self.process["start_camera_control"].poll() is None:
             input_data = data + '\n'  # Add a newline character at the end, because readline() reads until it encounters a newline
-            self.process.stdin.write(input_data.encode())  # stdin expects bytes, so encode the string as bytes
-            self.process.stdin.flush()  # flush the buffer to make sure the data is actually sent to the subprocess
+            self.process["start_camera_control"].stdin.write(input_data.encode())  # stdin expects bytes, so encode the string as bytes
+            self.process["start_camera_control"].stdin.flush()  # flush the buffer to make sure the data is actually sent to the subprocess
 
     def on_start_camera_stream(self):
         print('Received start camera stream command')
