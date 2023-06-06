4f79ca47b888d7611d7d45add31bde846601e1e5
timf34 <farrelti@tcd.ie>
2023-06-06 14:15:19 +0100

    added OS check to make it work on windows and jetson with the same file

--- jetson/jetson_stream_simulator.py
+++ jetson/jetson_stream_simulator.py
@@ -1,10 +1,18 @@
 import cv2
+import os 
 import requests 
 
-# Capture video from the webcam
-cap = cv2.VideoCapture(f'nvarguscamerasrc !  video/x-raw(memory:NVMM), width=1920, height=1080, format=NV12, framerate=60/1 ! '
-                f'nvvidconv ! video/x-raw, width={str(640)}, height={str(640)}, format=BGRx ! '
-                f'videoconvert ! video/x-raw, format=BGR ! appsink')
+
+# Check the operating system
+if os.name == 'nt':
+    cap = cv2.VideoCapture(0)  # Windows 
+else:
+    cap = cv2.VideoCapture(
+        f'nvarguscamerasrc !  video/x-raw(memory:NVMM), width=1920, height=1080, format=NV12, framerate=60/1 ! '
+        f'nvvidconv ! video/x-raw, width={str(640)}, height={str(640)}, format=BGRx ! '
+        f'videoconvert ! video/x-raw, format=BGR ! appsink'
+    )
+
 
 while True:
     # Read a frame from the camera
