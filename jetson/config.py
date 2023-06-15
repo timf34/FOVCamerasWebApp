ef65c5e1990e262da373725d6f20fc708b03415e
timf34 <farrelti@tcd.ie>
2023-06-15 23:49:23 +0100

    added record_video.py script and associated code files

---
+++ jetson/config.py
@@ -0,0 +1,41 @@
+"""
+A config file to help speed up match day work.
+For storing the time of the match + IP addresses too potentially.
+"""
+import os
+
+
+class BohsConfig:
+    def __init__(self):
+
+        if os.name == 'nt':
+            self.jetson_name: str = "jetson1"  # If we're on windows, just assume we're on jetson1
+        else:
+            self.jetson_name: str = os.environ.get('JETSON_NAME')
+
+        self.jetson_number: str = self.jetson_name[-1]
+        self.hour: int = 17
+        self.minute: int = 1
+        self.second: int = 2
+        self.microsecond: int = 1
+
+        assert 0 <= self.hour <= 23, "Hour must be between 0 and 23"
+        assert isinstance(self.hour, int), "Hour must be an int"
+        assert 0 <= self.minute <= 59 and isinstance(self.minute, int), "Minute must be between 0 and 59"
+        assert isinstance(self.minute, int), "Minute must be an int"
+        assert 0 <= self.second <= 59, "Second must be between 0 and 59"
+        assert isinstance(self.second, int), "Second must be an int"
+        assert isinstance(self.microsecond, int), "Microsecond must be an int"
+
+    def __str__(self):
+        return f"hour: {self.hour}, minute: {self.minute}, second: {self.second}, microsecond: {self.microsecond}"
+
+
+def _test():
+    x = BohsConfig()
+    print(x)
+    print(x.hour)
+
+
+if __name__ == "__main__":
+    _test()
---
+++ jetson/utils/utility_funcs.py
@@ -0,0 +1,51 @@
+import json
+import os
+import socket
+import time
+
+from datetime import datetime
+from typing import Dict, List
+
+
+def get_log_file_path(jetson_name: str) -> str:
+    """
+    Returns the path to the log file
+
+    Args:
+        jetson_name (str): The name of the jetson device
+    """
+    today = datetime.now()
+    if os.name == 'nt':
+        log_dir = f"{os.getcwd()}/logs/laptop"
+    else:
+        log_dir = f"{os.getcwd()}/logs/{jetson_name}"
+    check_and_create_dir(log_dir)
+    return f"{log_dir}/{today.strftime('%d_%m_%Y')}.log"
+
+
+def get_ip_address() -> str:
+    """Get the IP address of the device"""
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    return s.getsockname()[0]
+
+
+def save_to_json_file(data: Dict[str, List], dir: str = f"{os.getcwd()}/logs/jetson3") -> None:
+    """Save the data to a json file"""
+    filename = time.strftime("%H%M-%d_%m_%Y") + ".json"
+    check_and_create_dir(dir)
+    full_path = os.path.join(dir, filename)  # Create the full path to the file
+
+    with open(full_path, "w") as f:
+        json.dump(data, f)
+
+
+def check_and_create_dir(dir_name: str) -> None:
+    """Check if the directory exists and create it if it doesn't"""
+    if not os.path.exists(dir_name):
+        os.makedirs(dir_name)
+        print(f"Created directory: {dir_name}")
+    else:
+        print(f"Directory {dir_name} already exists")
+
+
