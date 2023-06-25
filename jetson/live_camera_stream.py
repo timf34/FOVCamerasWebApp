1a9b85c2fdf76e1791d0959cc49efb352abee4ca
timf34 <farrelti@tcd.ie>
2023-06-26 00:32:05 +0100

    reverted to old raw http method as opposed to using websockets for streaming

--- jetson/live_camera_stream.py
+++ jetson/live_camera_stream.py
@@ -1,40 +1,33 @@
 import cv2
-import os 
-import socketio
+import os
+import requests
 from utils.utility_funcs import load_env
-
 
 load_env()
 URL = os.environ.get('REACT_APP_URL')
 
 # Check the operating system
 if os.name == 'nt':
-    cap = cv2.VideoCapture(0)  # Windows 
+    cap = cv2.VideoCapture(0)  # Windows
 else:
     cap = cv2.VideoCapture(
         f'nvarguscamerasrc !  video/x-raw(memory:NVMM), width=1920, height=1080, format=NV12, framerate=60/1 ! '
         f'nvvidconv ! video/x-raw, width={str(640)}, height={str(640)}, format=BGRx ! '
         f'videoconvert ! video/x-raw, format=BGR ! appsink'
     )
-sio = socketio.Client()
-
-@sio.event
-def connect():
-    print("Connected to the server.")
-
-@sio.event
-def disconnect():
-    print("Disconnected from the server.")
-
-sio.connect(URL)  # replace with the URL of your server
 
 while True:
+    # Read a frame from the camera
+    print("Reading frame...")
     ret, frame = cap.read()
+
     if not ret:
         print("Failed to read frame.")
         break
 
+    # Encode the frame as JPEG
     ret, jpeg = cv2.imencode('.jpg', frame)
+
     if not ret:
         print("Failed to encode frame.")
         break
@@ -44,6 +37,10 @@ while True:
         print("Received 'q' key press. Exiting...")
         break
 
-    sio.emit('frame', {'image': jpeg.tobytes()})
+    # Send the JPEG image to the server
+    response = requests.post(f'{URL}/api/image', data=jpeg.tobytes(), headers={'content-type': 'image/jpeg'})
 
-sio.disconnect()
+    if response.status_code != 200:
+        print("Failed to send frame.")
+        print("Response code: ", response.status_code)
+        break
