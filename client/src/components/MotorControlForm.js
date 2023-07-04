f925194ccc35e3ce6e59b1f24d1cb0ad9b8d16e9
timf34 <farrelti@tcd.ie>
2023-07-04 13:16:34 +0100

    added percentages option to MotorControlForm.js

--- client/src/components/MotorControlForm.js
+++ client/src/components/MotorControlForm.js
@@ -4,7 +4,10 @@ import { auth } from './firebase';
 export default function MotorControlForm() {
     const [deviceId, setDeviceId] = useState('jetson1'); 
     const [axis, setAxis] = useState('');
-    const [percentage, setPercentage] = useState('');
+    const [steps, setSteps] = useState('');
+    const [usePercentage, setUsePercentage] = useState(false);
+
+    const maxSteps = { "f,": 9353, "i,": 75, "z,": 4073 };
 
     const moveMotor = async () => {
         if (!auth.currentUser) {
@@ -14,10 +17,18 @@ export default function MotorControlForm() {
         else {
             console.log('User logged in:', auth.currentUser.email);
         }
-        
+
         const token = await auth.currentUser.getIdToken();
 
         console.log('Token:', token);
+
+        // Calculate the steps or percentage depending on the selection
+        let calculatedSteps = steps;
+        if (usePercentage) {
+            calculatedSteps = Math.round((steps / 100) * maxSteps[axis]);
+        }
+
+        console.log("Calculated steps:", calculatedSteps);
 
         const response = await fetch(`${process.env.REACT_APP_URL}/api/send-input`, {
             method: 'POST',
@@ -25,7 +36,7 @@ export default function MotorControlForm() {
                 'Content-Type': 'application/json',
                 'Authorization': `Bearer ${token}`
             },
-            body: JSON.stringify({ deviceId, input: axis + percentage })
+            body: JSON.stringify({ deviceId, input: axis + calculatedSteps })
         });
 
         if (!response.ok) {
@@ -49,8 +60,16 @@ export default function MotorControlForm() {
                 </select>
             </label>
             <label>
-                Percentage:
-                <input type="number" value={percentage} onChange={(e) => setPercentage(e.target.value)} />
+                Mode:
+                <select value={usePercentage} onChange={(e) => setUsePercentage(e.target.value === 'true')}>
+                    <option value={false}>--Select mode--</option>
+                    <option value={false}>Steps</option>
+                    <option value={true}>Percentage</option>
+                </select>
+            </label>
+            <label>
+                Input Value:
+                <input type="number" value={steps} onChange={(e) => setSteps(e.target.value)} />
             </label>
             <label>
                 Select Device:
