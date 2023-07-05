71fb009b250e9a82ebda0a5431bc181eed2b2584
timf34 <farrelti@tcd.ie>
2023-07-05 13:10:56 +0100

    refactored MotorPositions.js to also use the globally selected deviceId

--- client/src/components/MotorPositions.js
+++ client/src/components/MotorPositions.js
@@ -1,15 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import { useForm } from './useForm';
+import deviceContext from "./DeviceContext";
 import '../stylesheets/MotorPositions.css'
+import DeviceContext from "./DeviceContext";
 
 function MotorPositions() {
-    const initialState = { selectedDevice: 'jetson1' };
-    const [values, handleChange] = useForm(initialState);
+    const { deviceId, setDeviceId } = useContext(DeviceContext);
     const [response, setResponse] = useState({});
 
     const fetchMotorPositions = () => {
-        const { selectedDevice } = values;
-        fetch(`${process.env.REACT_APP_URL}/api/get-motor-positions/${selectedDevice}`)
+        fetch(`${process.env.REACT_APP_URL}/api/get-motor-positions/${deviceId}`)
             .then(response => response.json())
             .then(data => setResponse(data))
             .catch((error) => console.error('Error:', error));
@@ -17,20 +17,11 @@ function MotorPositions() {
 
     useEffect(() => {
         fetchMotorPositions();
-    }, [values.selectedDevice]);
+    }, [deviceId]);
 
     return (
         <div className="container">
             <h5>Motor Positions</h5>
-            <div className="select">
-                <label>Select device: </label>
-                <select name="selectedDevice" value={values.selectedDevice} onChange={handleChange}>
-                    <option value="jetson1">jetson1</option>
-                    <option value="jetson2">jetson2</option>
-                    <option value="jetson3">jetson3</option>
-                    <option value="jetson4">jetson4</option>
-                </select>
-            </div>
             <div className="pre">
                 {Object.entries(response).map(([key, value], i) => (
                     <p key={i}><b>{key}:</b> {value}</p>
