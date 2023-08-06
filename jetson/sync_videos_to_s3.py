2db24e6294f4de0b2c164c42c98d9aa4ef964f1b
timf34 <farrelti@tcd.ie>
2023-08-06 01:05:16 +0100

    fixed path for syncing videos

--- jetson/sync_videos_to_s3.py
+++ jetson/sync_videos_to_s3.py
@@ -1,7 +1,7 @@
 import subprocess
 
 try:
-    result = subprocess.run(['aws', 's3', 'sync', './videos', 's3://australia-fov'], check=True)
+    result = subprocess.run(['aws', 's3', 'sync', '/home/fov/Desktop/videos', 's3://australia-fov'], check=True)
 except subprocess.CalledProcessError as e:
     # TODO: this should be logged to a log file that is synced to AWS
     print(f"Sync failed with error: {e}")
