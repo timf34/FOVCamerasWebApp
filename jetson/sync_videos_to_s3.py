54c1cb99f87f7437a916e16854d91f27d5df40f7
timf34 <farrelti@tcd.ie>
2023-08-06 01:00:28 +0100

    let the subprocess print to the terminal - not capturing the output in a variable

--- jetson/sync_videos_to_s3.py
+++ jetson/sync_videos_to_s3.py
@@ -1,8 +1,7 @@
 import subprocess
 
 try:
-    result = subprocess.run(['aws', 's3', 'sync', '/home/fov/Desktop/videos', 's3://australia-fov'], check=True, capture_output=True, text=True)
-    print(result.stdout)
+    result = subprocess.run(['aws', 's3', 'sync', './videos', 's3://australia-fov'], check=True)
 except subprocess.CalledProcessError as e:
     # TODO: this should be logged to a log file that is synced to AWS
     print(f"Sync failed with error: {e}")
