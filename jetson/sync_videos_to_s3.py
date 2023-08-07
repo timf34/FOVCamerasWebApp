e1a831521b81065c90c4a9e3449ea4315fe5c50c
timf34 <farrelti@tcd.ie>
2023-08-07 15:27:03 +0100

    added env to sync s3

--- jetson/sync_videos_to_s3.py
+++ jetson/sync_videos_to_s3.py
@@ -8,9 +8,9 @@ my_env["PATH"] = "/home/fov/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:
 try:
     if os.name == 'nt':
         # Windows
-        result = subprocess.run(['aws', 's3', 'sync', './videos', 's3://australia-fov'], check=True, env=my_env)
+        result = subprocess.run(['aws', 's3', 'sync', './videos', 's3://australia-fov'], check=True)
     else:
-        result = subprocess.run(['aws', 's3', 'sync', '/home/fov/Desktop/videos', 's3://australia-fov'], check=True)
+        result = subprocess.run(['aws', 's3', 'sync', '/home/fov/Desktop/videos', 's3://australia-fov'], check=True, env=my_env)
 except subprocess.CalledProcessError as e:
     # TODO: this should be logged to a log file that is synced to AWS
     print(f"Sync failed with error: {e}")
