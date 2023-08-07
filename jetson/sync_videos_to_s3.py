fe1a3a3d4859191c38f6545828ba6f9f80af4a24
timf34 <farrelti@tcd.ie>
2023-08-07 15:25:46 +0100

    added env

--- jetson/sync_videos_to_s3.py
+++ jetson/sync_videos_to_s3.py
@@ -1,10 +1,14 @@
 import os
 import subprocess
 
+my_env = os.environ.copy()
+my_env["PATH"] = "/home/fov/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"
+
+
 try:
     if os.name == 'nt':
         # Windows
-        result = subprocess.run(['aws', 's3', 'sync', './videos', 's3://australia-fov'], check=True)
+        result = subprocess.run(['aws', 's3', 'sync', './videos', 's3://australia-fov'], check=True, env=my_env)
     else:
         result = subprocess.run(['aws', 's3', 'sync', '/home/fov/Desktop/videos', 's3://australia-fov'], check=True)
 except subprocess.CalledProcessError as e:
